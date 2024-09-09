dfc34d7c86bd884d2d821846c00f895f1e7e80e2
yeowul <dldudnf0404@gmail.com>
2024-09-09 15:19:10 +0900

    commit

--- mysql/process.php
+++ mysql/process.php
@@ -1,19 +1,19 @@
 <?php
-    mysqli_connect('localhost','root',''); //(host,username,pw)
-    mysqli_select_db('opentutorials');
+    $con=mysqli_connect('localhost','root',''); //(host,username,pw)
+    mysqli_select_db($con,'opentutorals');
     switch($_GET['mode']){
         case 'insert':
-            $sql="INSERT INTO topic (title, description, created) VALUES ('".mysqli_real_escape_string($_POST['title'])."', '".mysqli_real_escape_string($_POST['description'])."', now())";
+            $sql="INSERT INTO topic (title, description, created) VALUES ('".mysqli_real_escape_string($con,$_POST['title'])."', '".mysqli_real_escape_string($con,$_POST['description']."', now())");
             echo $sql;
-            $result = mysqli_query($sql);
+            $result = mysqli_query($con,$sql);
             // header("Location: list.php"); 
             break;
         case 'delete':
-        mysqli_query('DELETE FROM topic WHERE id = '.mysqli_real_escape_string($_POST['id']));
-        header("Location: list.php"); 
+        mysqli_query($con,'DELETE FROM topic WHERE id = '.mysqli_real_escape_string($con,$_POST['id']));
+        header("Location: list.php");
         break;
         case 'modify':
-        mysqli_query('UPDATE topic SET title = "'.mysql_reali_escape_string($_POST['title']).'", description = "'.mysqli_real_escape_string($_POST['description']).'" WHERE id = '.mysqli_real_escape_string($_POST['id']));
+        mysqli_query($con,'UPDATE topic SET title = "'.mysqli_real_escape_string($con,$_POST['title']).'", description = "'.mysqli_real_escape_string($con,$_POST['description']).'" WHERE id = '.mysqli_real_escape_string($con,$_POST['id']));
         header("Location: list.php?id={$_POST['id']}");
         break;
     }
